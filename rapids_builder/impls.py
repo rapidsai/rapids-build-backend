95e2c279bf25d2660bd59b265e1770496a7825c4
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-12 17:31:01 +0000

    First pass at using config in code

--- rapids_builder/impls.py
+++ rapids_builder/impls.py
@@ -12,6 +12,7 @@ import tomli_w
 from packaging.requirements import Requirement
 from packaging.specifiers import SpecifierSet
 
+from .config import Config
 from .utils import _get_pyproject
 
 
@@ -45,42 +46,51 @@ def _get_backend():
         )
 
 
-@lru_cache(1)
-def _get_cuda_major():
+@lru_cache(2)
+def _get_cuda_major(allow_no_cuda=False):
     """Get the CUDA suffix based on nvcc.
 
-    Returns None if nvcc is not in the PATH.
+    Parameters
+    ----------
+    allow_no_cuda : bool
+        If True, return None if nvcc is not in the PATH. If False, raise an exception.
     """
-    nvcc_exists = subprocess.run(["which", "nvcc"], capture_output=True).returncode == 0
-    if not nvcc_exists:
-        if not _get_tool_table().get("allow-no-cuda", False):
+    try:
+        nvcc_exists = (
+            subprocess.run(["which", "nvcc"], capture_output=True).returncode == 0
+        )
+        if not nvcc_exists:
             raise ValueError(
                 "Could not determine the CUDA version. Make sure nvcc is in your PATH."
             )
-        return None
 
-    try:
-        process_output = subprocess.run(["nvcc", "--version"], capture_output=True)
-    except subprocess.CalledProcessError as e:
-        raise ValueError("Failed to get version from nvcc.") from e
+        try:
+            process_output = subprocess.run(["nvcc", "--version"], capture_output=True)
+        except subprocess.CalledProcessError as e:
+            raise ValueError("Failed to get version from nvcc.") from e
 
-    output_lines = process_output.stdout.decode().splitlines()
+        output_lines = process_output.stdout.decode().splitlines()
 
-    try:
         match = re.search(r"release (\d+)", output_lines[3])
+        if match is None:
+            raise ValueError("Failed to parse CUDA version from nvcc output.")
         return match.group(1)
-    except BaseException as e:
-        raise ValueError(
-            "Failed to parse CUDA version from nvcc --version output."
-        ) from e
+    except Exception:
+        if allow_no_cuda:
+            return None
+        raise
 
 
-def _get_cuda_suffix():
+def _get_cuda_suffix(allow_no_cuda=False):
     """Get the CUDA suffix based on nvcc.
 
-    Returns an empty string if nvcc is not in the PATH.
+    Parameters
+    ----------
+    allow_no_cuda : bool
+        If True, return an empty string if CUDA could not be detected. If False, raise
+        an exception.
     """
-    if (major := _get_cuda_major()) is None:
+    if (major := _get_cuda_major(allow_no_cuda)) is None:
         return ""
     return f"-cu{major}"
 
@@ -126,17 +136,17 @@ _CUDA_11_ONLY_WHEELS = (
 )
 
 
-def _suffix_requires(requires):
+def _suffix_requires(requires, allow_no_cuda, only_release_deps):
     """Add the CUDA suffix to any versioned RAPIDS wheels in requires."""
     new_requires = []
-    suffix = _get_cuda_suffix()
+    suffix = _get_cuda_suffix(allow_no_cuda)
     for req in requires:
         req = Requirement(req)
 
         # cupy is a special case because it's not a RAPIDS wheel. If we can't
         # determine the local CUDA version, then we fall back to making the sdist of
         # cupy on PyPI the dependency.
-        major = _get_cuda_major()
+        major = _get_cuda_major(allow_no_cuda)
         if req.name == "cupy" and major is not None:
             req.name += f"-cuda{major}x"
         else:
@@ -150,7 +160,6 @@ def _suffix_requires(requires):
             is_unversioned_wheel = any(
                 req.name == w for w in _UNVERSIONED_RAPIDS_WHEELS
             )
-            only_release_deps = "RAPIDS_ONLY_RELEASE_DEPS" in os.environ
 
             if is_versioned_wheel:
                 req.name += suffix
@@ -166,21 +175,6 @@ def _suffix_requires(requires):
 
         new_requires.append(str(req))
     return new_requires
-
-
-def _supplement_requires(getter, config_settings):
-    """Add to the list of requirements for the build backend.
-
-    This is used to add the requirements specified in the rapids_builder table.
-    """
-    try:
-        requires = _suffix_requires(_get_tool_table()["requires"])
-    except KeyError:
-        requires = []
-
-    if (getter := getattr(_get_backend(), getter, None)) is not None:
-        requires.extend(getter(config_settings))
-    return requires
 
 
 @lru_cache(1)
@@ -202,17 +196,17 @@ def _get_git_commit():
 
 
 @contextmanager
-def _edit_git_commit():
+def _edit_git_commit(config):
     """
     Temporarily modify the git commit of the package being built.
 
     This is useful for projects that want to embed the current git commit in the package
     at build time.
     """
-    commit_file = _get_tool_table().get("commit-file")
+    commit_file = config.commit_file
     commit = _get_git_commit()
 
-    if commit_file is not None and commit is not None:
+    if commit_file != "" and commit is not None:
         bkp_commit_file = f".{os.path.basename(commit_file)}.rapids_builder.bak"
         try:
             with open(commit_file) as f:
@@ -241,7 +235,7 @@ def _edit_git_commit():
 
 
 @contextmanager
-def _edit_pyproject():
+def _edit_pyproject(config):
     """
     Temporarily modify the name and dependencies of the package being built.
 
@@ -251,16 +245,20 @@ def _edit_pyproject():
     """
     pyproject = _get_pyproject()
     project_data = pyproject["project"]
-    project_data["name"] += _get_cuda_suffix()
+    project_data["name"] += _get_cuda_suffix(config)
 
     dependencies = pyproject["project"].get("dependencies")
     if dependencies is not None:
-        project_data["dependencies"] = _suffix_requires(project_data["dependencies"])
+        project_data["dependencies"] = _suffix_requires(
+            project_data["dependencies"], config.allow_no_cuda, config.only_release_deps
+        )
 
     optional_dependencies = pyproject["project"].get("optional-dependencies")
     if optional_dependencies is not None:
         project_data["optional-dependencies"] = {
-            extra: _suffix_requires(deps)
+            extra: _suffix_requires(
+                deps, config.allow_no_cuda, config.only_release_deps
+            )
             for extra, deps in optional_dependencies.items()
         }
 
@@ -284,29 +282,55 @@ def _edit_pyproject():
 # backend's hooks to the rapids_builder hooks, but that's not a big deal because these
 # functions only executed by the build frontend and are not user-facing.
 def get_requires_for_build_wheel(config_settings):
-    with _edit_pyproject(), _edit_git_commit():
-        return _supplement_requires("get_requires_for_build_wheel", config_settings)
+    config = Config()
+    with _edit_pyproject(config), _edit_git_commit(config):
+        requires = _suffix_requires(
+            config.requires, config.allow_no_cuda, config.only_release_deps
+        )
+
+        if hasattr(backend := _get_backend(), "get_requires_for_build_wheel"):
+            requires.extend(backend.get_requires_for_build_wheel(config_settings))
+
+        return requires
 
 
 def get_requires_for_build_sdist(config_settings):
-    with _edit_pyproject(), _edit_git_commit():
-        return _supplement_requires("get_requires_for_build_sdist", config_settings)
+    config = Config()
+    with _edit_pyproject(config), _edit_git_commit(config):
+        requires = _suffix_requires(
+            config.requires, config.allow_no_cuda, config.only_release_deps
+        )
+
+        if hasattr(backend := _get_backend(), "get_requires_for_build_sdist"):
+            requires.extend(backend.get_requires_for_build_sdist(config_settings))
+
+        return requires
 
 
 def get_requires_for_build_editable(config_settings):
-    with _edit_pyproject(), _edit_git_commit():
-        return _supplement_requires("get_requires_for_build_editable", config_settings)
+    config = Config()
+    with _edit_pyproject(config), _edit_git_commit(config):
+        requires = _suffix_requires(
+            config.requires, config.allow_no_cuda, config.only_release_deps
+        )
+
+        if hasattr(backend := _get_backend(), "get_requires_for_build_editable"):
+            requires.extend(backend.get_requires_for_build_editable(config_settings))
+
+        return requires
 
 
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
-    with _edit_pyproject(), _edit_git_commit():
+    config = Config()
+    with _edit_pyproject(config), _edit_git_commit(config):
         return _get_backend().build_wheel(
             wheel_directory, config_settings, metadata_directory
         )
 
 
 def build_sdist(sdist_directory, config_settings=None):
-    with _edit_pyproject(), _edit_git_commit():
+    config = Config()
+    with _edit_pyproject(config), _edit_git_commit(config):
         return _get_backend().build_sdist(sdist_directory, config_settings)
 
 
@@ -314,21 +338,24 @@ def build_sdist(sdist_directory, config_settings=None):
 # These definitions assume that they will only be called if the wrapped backend
 # implements them by virtue of the logic in __init__.py.
 def build_editable(wheel_directory, config_settings=None, metadata_directory=None):
-    with _edit_pyproject(), _edit_git_commit():
+    config = Config()
+    with _edit_pyproject(config), _edit_git_commit(config):
         return _get_backend().build_editable(
             wheel_directory, config_settings, metadata_directory
         )
 
 
 def prepare_metadata_for_build_wheel(metadata_directory, config_settings=None):
-    with _edit_pyproject():
+    config = Config()
+    with _edit_pyproject(config):
         return _get_backend().prepare_metadata_for_build_wheel(
             metadata_directory, config_settings
         )
 
 
 def prepare_metadata_for_build_editable(metadata_directory, config_settings=None):
-    with _edit_pyproject():
+    config = Config()
+    with _edit_pyproject(config):
         return _get_backend().prepare_metadata_for_build_editable(
             metadata_directory, config_settings
         )
