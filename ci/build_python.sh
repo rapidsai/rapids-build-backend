7c5d0a3d7b9a61c892a12e1d462efc2d7d9de935
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-13 01:00:45 +0000

    Add conda recipe and corresponding CI build job

---
+++ ci/build_python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright (c) 2024, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+rapids-configure-conda-channels
+
+source rapids-date-string
+
+rapids-print-env
+
+rapids-logger "Begin py build"
+
+rapids-conda-retry mambabuild conda/recipes/rapids_build_backend
+
+rapids-upload-conda-to-s3 python
