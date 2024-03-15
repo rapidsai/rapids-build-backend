32baecbf2001a75dc15b1e950182e4927cd0cee5
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-15 22:25:10 +0000

    Specify the output directory

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -5,9 +5,10 @@ set -euo pipefail
 
 UPLOAD_PACKAGES="${1:-false}"
 
-conda mambabuild conda/recipes/rapids-build-backend
+PKG_DIR="${PWD}/dist/"
+conda mambabuild --output-folder "${PKG_DIR}" conda/recipes/rapids-build-backend
 
 if [ "$UPLOAD_PACKAGES" = "true" ]; then
     # TODO: Figure out the best way to get CONDA_PKG_FILE
-    rapids-retry anaconda -t "${RAPIDS_CONDA_TOKEN}" upload --label main --skip-existing --no-progress ${CONDA_PKG_FILE}
+    rapids-retry anaconda -t "${RAPIDS_CONDA_TOKEN}" upload --label main --skip-existing --no-progress "${PKG_DIR}/*"
 fi
