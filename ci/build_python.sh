fb78f254e73bd3f8cd9736dfa618ab523768fe65
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-15 22:12:02 +0000

    Enable uploading in nightly builds

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -3,4 +3,11 @@
 
 set -euo pipefail
 
+UPLOAD_PACKAGES="${1:-false}"
+
 conda mambabuild conda/recipes/rapids-build-backend
+
+if [ "$UPLOAD_PACKAGES" = "true" ]; then
+    # TODO: Figure out the best way to get CONDA_PKG_FILE
+    rapids-retry anaconda -t "${RAPIDS_CONDA_TOKEN}" upload --label main --skip-existing --no-progress ${CONDA_PKG_FILE}
+fi
--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -3,8 +3,15 @@
 
 set -euo pipefail
 
+UPLOAD_PACKAGES="${1:-false}"
+
 python -m pip wheel . -w dist -vv --no-deps --disable-pip-version-check
 
 # Run tests
-python -m pip install $(ls dist/*.whl)[test]
+WHL_FILE=$(ls dist/*.whl)
+python -m pip install "${WHL_FILE}[test]"
 python -m pytest -v tests/
+
+if [ "$UPLOAD_PACKAGES" = "true" ]; then
+    anaconda -t "${RAPIDS_CONDA_TOKEN}" upload --skip-existing --no-progress ${WHL_FILE}
+fi
