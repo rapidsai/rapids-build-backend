b6cb210970e6ba6aeba4ccbc8a66bdd55989c765
Bradley Dice <bdice@bradleydice.com>
2024-03-18 18:06:12 -0500

    Upload from noarch directory. (#14)
    This fixes conda package uploads, which are failing to find packages
    without the `noarch` directory.

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 
 UPLOAD_PACKAGES="${1:-false}"
 
-PKG_DIR="${PWD}/conda_package/"
+PKG_DIR="${PWD}/conda_package"
 rapids-conda-retry mambabuild --output-folder "${PKG_DIR}" conda/recipes/rapids-build-backend
 
 if [ "$UPLOAD_PACKAGES" = "true" ]; then
     # TODO: Figure out the best way to get CONDA_PKG_FILE
-    rapids-retry anaconda -t "${RAPIDS_CONDA_TOKEN}" upload --label main --skip-existing --no-progress "${PKG_DIR}/*"
+    rapids-retry anaconda -t "${RAPIDS_CONDA_TOKEN}" upload --label main --skip-existing --no-progress "${PKG_DIR}/noarch/*.tar.bz2"
 fi
