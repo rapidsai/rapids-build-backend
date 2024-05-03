8a33c74400b565ded0a2c9ce56ce2fd008ad371c
Kyle Edwards <kyedwards@nvidia.com>
2024-05-03 16:20:21 -0400

    Use CI matrix from shared-workflows

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -7,8 +7,3 @@ UPLOAD_PACKAGES="${1:-false}"
 
 PKG_DIR="${PWD}/conda_package"
 rapids-conda-retry mambabuild --output-folder "${PKG_DIR}" conda/recipes/rapids-build-backend
-
-if [ "$UPLOAD_PACKAGES" = "true" ]; then
-    # TODO: Figure out the best way to get CONDA_PKG_FILE
-    rapids-retry anaconda -t "${RAPIDS_CONDA_TOKEN}" upload --label main --skip-existing --no-progress "${PKG_DIR}/noarch/"*.tar.bz2
-fi
--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -3,15 +3,9 @@
 
 set -euo pipefail
 
-UPLOAD_PACKAGES="${1:-false}"
-
 python -m pip wheel . -w dist -vv --no-deps --disable-pip-version-check
 
 # Run tests
 WHL_FILE=$(ls dist/*.whl)
 python -m pip install "${WHL_FILE}[test]"
 python -m pytest -v tests/
-
-if [ "$UPLOAD_PACKAGES" = "true" ]; then
-    anaconda -t "${RAPIDS_CONDA_TOKEN}" upload --skip-existing --no-progress ${WHL_FILE}
-fi
