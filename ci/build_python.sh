7e1e2cd6d031bdde0d4ca3e0c7fe93a4ed186e2b
Kyle Edwards <kyedwards@nvidia.com>
2024-05-06 17:38:18 -0400

    Upload to S3

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -3,5 +3,6 @@
 
 set -euo pipefail
 
-PKG_DIR="${PWD}/conda_package"
-rapids-conda-retry mambabuild --output-folder "${PKG_DIR}" conda/recipes/rapids-build-backend
+rapids-conda-retry mambabuild conda/recipes/rapids-build-backend
+
+rapids-upload-conda-to-s3 python
--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -9,3 +9,5 @@ python -m pip wheel . -w dist -vv --no-deps --disable-pip-version-check
 WHL_FILE=$(ls dist/*.whl)
 python -m pip install "${WHL_FILE}[test]"
 python -m pytest -v tests/
+
+rapids-upload-wheels-to-s3 dist
