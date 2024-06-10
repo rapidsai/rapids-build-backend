1a5f6f81dd0d08c1e509a464de309b13d6c10d8d
James Lamb <jaylamb20@gmail.com>
2024-06-10 15:48:11 -0500

    Update tests/examples/setuptools-with-imports-in-setup-py/setup.py
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- tests/examples/setuptools-with-imports-in-setup-py/setup.py
+++ tests/examples/setuptools-with-imports-in-setup-py/setup.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
-# Note that 'matplotlib is not listed anywhere in pyproject.toml.
+# Note that matplotlib is not listed anywhere in pyproject.toml.
 # This import can only succeed if rapids-build-backend successfully identified it as
 # a build-time dependency via dependencies.yaml.
 import matplotlib
