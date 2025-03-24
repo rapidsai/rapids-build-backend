cd18784c24847cb84957aab47d5f84d784bbe026
VenkateshJaya <jjayabaskar@nvidia.com>
2025-03-24 10:25:40 -0700

    added wheel location

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -3,11 +3,13 @@
 
 set -euo pipefail
 
-python -m pip wheel . -w dist -vv --no-deps --disable-pip-version-check
+wheel_dir=${RAPIDS_WHEEL_BLD_OUTPUT_DIR}
+
+python -m pip wheel . -w "${wheel_dir}" -vv --no-deps --disable-pip-version-check
 
 # Run tests
-WHL_FILE=$(ls dist/*.whl)
+WHL_FILE=$(ls "${wheel_dir}"/*.whl)
 python -m pip install "${WHL_FILE}[test]"
 python -m pytest -v tests/
 
-RAPIDS_PY_WHEEL_NAME="rapids-build-backend" rapids-upload-wheels-to-s3 dist
+RAPIDS_PY_WHEEL_NAME="rapids-build-backend" rapids-upload-wheels-to-s3 "${wheel_dir}"
