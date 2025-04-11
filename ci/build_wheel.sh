dfba7bb498372ef8f28ebbf7e73fffbf72752edb
Jaya Venkatesh <jjayabaskar@nvidia.com>
2025-04-11 14:30:09 -0700

    Moving wheel builds to specified location and uploading build artifacts to Github (#63)

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
 set -euo pipefail
 
-python -m pip wheel . -w dist -vv --no-deps --disable-pip-version-check
+python -m pip wheel . -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" -vv --no-deps --disable-pip-version-check
 
 # Run tests
-WHL_FILE=$(ls dist/*.whl)
+WHL_FILE=$(ls "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"/*.whl)
 python -m pip install "${WHL_FILE}[test]"
 python -m pytest -v tests/
 
-RAPIDS_PY_WHEEL_NAME="rapids-build-backend" rapids-upload-wheels-to-s3 dist
+RAPIDS_PY_WHEEL_NAME="rapids-build-backend" rapids-upload-wheels-to-s3 "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
