2875bc9b6d9e871afcc1a4e8e3ea6848650f98cb
James Lamb <jlamb@nvidia.com>
2026-01-29 11:48:11 -0600

    enforce wheel size limits, PEP 639 metadata

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 
 python -m pip wheel . -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" -vv --no-deps --disable-pip-version-check
 
+ci/validate_wheel.sh "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
 # Run tests
 WHL_FILE=$(ls "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"/*.whl)
 python -m pip install "${WHL_FILE}[test]"
