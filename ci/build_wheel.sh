4867abc88b059a2398fa23dcc2e98882ada16969
James Lamb <jaylamb20@gmail.com>
2026-01-30 14:45:40 -0600

    enforce wheel size limits, PEP 639 metadata (#78)
    ## Description
    
    Proposes a batch of miscellaneous build / packaging / CI changes.
    
    ## Changes
    
    ### Adds size limit enforcement
    
    Contributes to https://github.com/rapidsai/build-planning/issues/219
    
    Adds some wheel validation used across most of RAPIDS:
    
    * package size is not too large (`pydistcheck`)
    * metadata is well-formatted (`twine`)
    
    ### Enforces PEP 639 license metadata in `pyproject.toml`
    
    Contributes to https://github.com/rapidsai/pre-commit-hooks/issues/95
    
    ### Updates `shared-workflows` branch to `main`
    
    They were pointing to `branch-25.10`.
    
    We're building a single noarch Python package here... it should always
    be safe to track `main` of `shared-workflows.
    
    Doing that will ensure this repo has access to the latest CI images,
    which have the latest preferred build tools and copy of
    https://github.com/rapidsai/gha-tools

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
 
 python -m pip wheel . -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" -vv --no-deps --disable-pip-version-check
+
+ci/validate_wheel.sh "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
 
 # Run tests
 WHL_FILE=$(ls "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"/*.whl)
